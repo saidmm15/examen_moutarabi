8fe7e98f4925250cd41fcfa9d77e77ab391a870d
Said <said.xtrategics@gmail.com>
2020-02-28 09:43:06 +0100

    versio 2

--- examen_moutarabi/src/examen_moutarabi/exercici3.java
+++ examen_moutarabi/src/examen_moutarabi/exercici3.java
@@ -4,15 +4,9 @@ public class exercici3 {
 	public static void main(String[] args) {
 		printOne();
 		printOne();
-		printTwo();
-		
 	}
 	public static void printOne() {
 		System.out.println("Hello world");
-	}
-	public static void printTwo() {
-		printOne();
-		printOne();
 	}
 }
 
